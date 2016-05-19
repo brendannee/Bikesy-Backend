a0b07606840933bf1e63857d2278e77fe29bf80d
Brendan Nee <brendan@automatic.com>
2016-05-19 12:16:04 +0200

    Cleanup of compile_graph

--- pygs/graphserver/compiler/compile_graph.py
+++ pygs/graphserver/compiler/compile_graph.py
@@ -6,27 +6,24 @@ from graphserver import compiler
 from graphserver.core import Graph
 import sys
 from sys import argv
-    
+
 def process_street_graph(osmdb_filename, graphdb_filename, profiledb_filename, slogs={}, cycleslogs={}, bicycleslogs={}, routeslogs={}, accessslogs={}):
-    OSMDB_FILENAME = "ext/osm/bartarea.sqlite"
-    GRAPHDB_FILENAME = "bartstreets.db"
-    
     print( "Opening OSM-DB '%s'"%osmdb_filename )
     osmdb = OSMDB( osmdb_filename )
-    
+
     if profiledb_filename:
         print( "Opening ProfileDB '%s'"%profiledb_filename )
         profiledb = ProfileDB( profiledb_filename )
     else:
         print( "No ProfileDB supplied" )
         profiledb = None
-    
+
     g = Graph()
     compiler.load_streets_to_graph( g, osmdb, profiledb, slogs, cycleslogs, bicycleslogs, routeslogs, accessslogs, reporter=sys.stdout )
-    
+
     graphdb = GraphDatabase( graphdb_filename, overwrite=True )
     graphdb.populate( g, reporter=sys.stdout )
-    
+
 def process_transit_graph(graphdb_filename, gtfsdb_filenames, osmdb_filename=None, profiledb_filename=None, agency_id=None, link_stations=False, slogs={}, cycleslogs={}, bicycleslogs={}, routeslogs={}, accessslogs={}):
     g = Graph()
 
@@ -42,21 +39,21 @@ def process_transit_graph(graphdb_filename, gtfsdb_filenames, osmdb_filename=Non
         print( "Opening OSM-DB '%s'"%osmdb_filename )
         osmdb = OSMDB( osmdb_filename )
         compiler.load_streets_to_graph( g, osmdb, profiledb, slogs, cycleslogs, bicycleslogs, routeslogs, accessslogs, reporter=sys.stdout )
-    
+
     # Load gtfsdb ==============================
-   
-    for i, gtfsdb_filename in enumerate(gtfsdb_filenames): 
+
+    for i, gtfsdb_filename in enumerate(gtfsdb_filenames):
         gtfsdb = GTFSDatabase( gtfsdb_filename )
         service_ids = [x.encode("ascii") for x in gtfsdb.service_ids()]
         compiler.load_gtfsdb_to_boardalight_graph(g, str(i), gtfsdb, agency_id=agency_id, service_ids=service_ids)
         if osmdb_filename:
             compiler.load_transit_street_links_to_graph( g, osmdb, gtfsdb, reporter=sys.stdout )
-        
+
         if link_stations:
             compiler.link_nearby_stops( g, gtfsdb )
 
     # Export to graphdb ========================
-    
+
     graphdb = GraphDatabase( graphdb_filename, overwrite=True )
     graphdb.populate( g, reporter=sys.stdout )
 
@@ -93,7 +90,7 @@ def main():
                       help="specify access for access, in acess:slog form. For example, 'access:1000'")
 
     (options, args) = parser.parse_args()
-    
+
     slogs = {}
     for slog_string in options.slog_strings:
         highway_type,slog_penalty = slog_string.split(":")
@@ -104,27 +101,27 @@ def main():
     for cycle_string in options.cycle_strings:
         cycleway_type,cycleway_penalty = cycle_string.split(":")
         cycleslogs[cycleway_type] = float(cycleway_penalty)
-    print cycleslogs 
+    print cycleslogs
 
     bicycleslogs = {}
     for bicycle_string in options.bicycle_strings:
         bicycle_type,bicycle_penalty = bicycle_string.split(":")
         bicycleslogs[bicycle_type] = float(bicycle_penalty)
-    print bicycleslogs 
+    print bicycleslogs
 
     routeslogs = {}
     for route_string in options.route_strings:
         route_type,route_penalty = route_string.split(":")
         routeslogs[route_type] = float(route_penalty)
-    print routeslogs 
-    
+    print routeslogs
+
     accessslogs = {}
     for access_string in options.access_strings:
         access_type,access_penalty = access_string.split(":")
         accessslogs[access_type] = float(access_penalty)
-    print accessslogs 
-    
-    
+    print accessslogs
+
+
     if len(args) != 1 or not options.osmdb_filename and not len(options.gtfsdb_files):
         #print len(args)
         parser.print_help()
@@ -136,12 +133,12 @@ def main():
     if options.osmdb_filename and not len(options.gtfsdb_files):
         process_street_graph(options.osmdb_filename, graphdb_filename, options.profiledb_filename, slogs, cycleslogs, bicycleslogs, routeslogs, accessslogs)
         exit(0)
-    
-    
+
+
     process_transit_graph(graphdb_filename, options.gtfsdb_files,
                           osmdb_filename=options.osmdb_filename,
                           profiledb_filename=options.profiledb_filename,
                           link_stations=options.link and not options.osmdb_filename, slogs=slogs, cycleslogs=cycleslogs, bicycleslogs=bicycleslogs, routeslogs=routeslogs, accessslogs=accessslogs)
     exit(0)
-        
+
 if __name__=='__main__': main()
